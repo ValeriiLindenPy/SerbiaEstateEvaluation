5ebb5645a9b4da99b2bc1252604948fa4e8ac370
ValeriiLindenPy <115076024+ValeriiLindenPy@users.noreply.github.com>
2024-04-06 14:14:40 +0300

    UI version01

---
+++ server/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+from utils import load_externals, predict_price_from_input, get_parametral_data
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Load externals
+load_externals()
+
+class PredictForm(FlaskForm):
+    district_list, rooms_number_list = get_parametral_data()
+    district_choices = [(district, district) for district in district_list]
+    num_rooms_choices = [(str(room), str(room)) for room in rooms_number_list]
+    district = SelectField('District', choices=district_choices, validators=[DataRequired()])
+    num_rooms = SelectField('Number of Rooms', choices=num_rooms_choices, validators=[DataRequired()])
+    size_sqm = FloatField('Size (m²)', validators=[DataRequired()])
+    submit = SubmitField('Predict')
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    form = PredictForm()
+    predicted_price = None
+    
+    if form.validate_on_submit():
+        district = form.district.data
+        num_rooms = form.num_rooms.data
+        size_sqm = form.size_sqm.data
+        
+        predicted_price = predict_price_from_input(district, num_rooms, size_sqm)
+    
+    # Fetch data for SelectFields
+    district_list, rooms_number_list = get_parametral_data()
+    
+    # Populate SelectFields
+    form.district.choices = [(district, district) for district in district_list]
+    form.num_rooms.choices = [(str(room), str(room)) for room in rooms_number_list]
+    
+    return render_template('predict.html', form=form, predicted_price=predicted_price)
+
+
+if __name__ == '__main__':  
+   app.run(debug=True)
---
+++ server/utils.py
@@ -0,0 +1,61 @@
+# server/utils.py
+import pandas as pd
+from joblib import load
+from pathlib import Path  # Importing pathlib
+
+
+# Constructing the path to district_data.csv
+current_directory = Path(__file__)
+district_data_path = current_directory.parent / 'district-data.csv'
+model_path = current_directory.parent / 'flatprice-predictor.joblib'
+
+# Load externals
+
+__district_data = None
+__model = None
+
+def load_externals():
+    global __district_data
+    global __model
+    __district_data = pd.read_csv(district_data_path)
+    __model = load(model_path)
+
+def predict_price_from_input(district: str, num_rooms: float, size_sqm: float):
+    global __model
+    global __district_data
+
+    district = district.lower().strip()
+
+    district_df = pd.DataFrame(columns=__district_data.columns)
+    district_df.loc[0] = 0  
+    if district in __district_data.columns:
+        district_df[district] = 1  
+    if 'Unnamed: 0' in district_df.columns:
+        district_df.drop('Unnamed: 0', axis=1, inplace=True)
+
+    # Make prediction
+    input_data = pd.DataFrame({
+        'Number of rooms': [num_rooms],
+        'Size (m²)': [size_sqm]
+    })
+    input_data = pd.concat([input_data, district_df], axis=1)
+    predicted_price = __model.predict(input_data)
+
+    return round(predicted_price[0], 2)
+
+
+def get_parametral_data():
+    global __district_data
+
+    district_df = __district_data.copy()
+    district_list = district_df.columns.tolist()[1:]
+    rooms_number_list = [0.5,1,1.5,2,2.5,3,3.5,4,4.5,5]
+    return district_list, rooms_number_list
+
+    
+
+
+if __name__ == '__main__':
+   load_externals()  
+   get_parametral_data()
+
